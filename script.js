507f923bc5b7a32f7f3e48ba13384055d9634823
zenos0x <nivashnandha2002@gmail.com>
2025-10-07 17:04:37 +0530

    spin with 10 options

--- script.js
+++ script.js
@@ -19,65 +19,58 @@ function shuffle(array) {
 }
 
 function spin() {
-  // Play the sound
+  const spinButton = document.querySelector('.spin');
+  spinButton.disabled = true;
+
   wheel.play();
-  // Inisialisasi variabel
   const box = document.getElementById("box");
   const element = document.getElementById("mainbox");
-  let SelectedItem = "";
 
-  // Shuffle 450 karena class box1 sudah ditambah 90 derajat diawal. minus 40 per item agar posisi panah pas ditengah.
-  // Setiap item memiliki 12.5% kemenangan kecuali item sepeda yang hanya memiliki sekitar 4% peluang untuk menang.
-  // Item berupa ipad dan samsung tab tidak akan pernah menang.
-  // let Sepeda = shuffle([2210]); //Kemungkinan : 33% atau 1/3
-  let MagicRoaster = shuffle([1890, 2250, 2610]);
-  let Sepeda = shuffle([1850, 2210, 2570]); //Kemungkinan : 100%
-  let RiceCooker = shuffle([1810, 2170, 2530]);
-  let LunchBox = shuffle([1770, 2130, 2490]);
-  let Sanken = shuffle([1750, 2110, 2470]);
-  let Electrolux = shuffle([1630, 1990, 2350]);
-  let JblSpeaker = shuffle([1570, 1930, 2290]);
+  // Angles for the center of each of the 10 wheel segments.
+  // Assumes even distribution (36 degrees per segment).
+  // Item 1 (French Fries) is assumed to be centered at 18 degrees.
+  const prize_map = {
+    "French fries": [18, 198],
+    "bao": [54, 234],
+    "milk shake": [90, 270],
+    "momos": [126, 306],
+    "wings": [162, 342],
+  };
 
-  // Bentuk acak
-  let Hasil = shuffle([
-    MagicRoaster[0],
-    Sepeda[0],
-    RiceCooker[0],
-    LunchBox[0],
-    Sanken[0],
-    Electrolux[0],
-    JblSpeaker[0],
-  ]);
-  // console.log(Hasil[0]);
+  const prizes = Object.keys(prize_map);
+  const selectedPrize = prizes[Math.floor(Math.random() * prizes.length)];
 
-  // Ambil value item yang terpilih
-  if (MagicRoaster.includes(Hasil[0])) SelectedItem = "Magic Roaster";
-  if (Sepeda.includes(Hasil[0])) SelectedItem = "Sepeda Aviator";
-  if (RiceCooker.includes(Hasil[0])) SelectedItem = "Rice Cooker Philips";
-  if (LunchBox.includes(Hasil[0])) SelectedItem = "Lunch Box Lock&Lock";
-  if (Sanken.includes(Hasil[0])) SelectedItem = "Air Cooler Sanken";
-  if (Electrolux.includes(Hasil[0])) SelectedItem = "Electrolux Blender";
-  if (JblSpeaker.includes(Hasil[0])) SelectedItem = "JBL Speaker";
+  const angles = prize_map[selectedPrize];
+  const prizeAngle = angles[Math.floor(Math.random() * angles.length)];
 
-  // Proses
+  // The arrow is on the left side, pointing to the 180-degree mark.
+  const arrowPosition = 0;
+  
+  // We rotate the wheel so the prizeAngle aligns with the arrowPosition.
+  let finalRotation = arrowPosition - prizeAngle;
+
+  // Add full rotations for the spinning effect.
+  finalRotation += 360 * (Math.floor(Math.random() * 5) + 5);
+
   box.style.setProperty("transition", "all ease 5s");
-  box.style.transform = "rotate(" + Hasil[0] + "deg)";
+  box.style.transform = "rotate(" + finalRotation + "deg)";
   element.classList.remove("animate");
+
   setTimeout(function () {
     element.classList.add("animate");
   }, 5000);
 
-  // Munculkan Alert
   setTimeout(function () {
     applause.play();
     swal(
       "Congratulations",
-      "You Won The " + SelectedItem + ".",
+      "You Won " + selectedPrize + ".",
       "success"
-    );
+    ).then(() => {
+      // spinButton.disabled = false;
+    });
   }, 5500);
 
-  // Delay and set to normal state
   setTimeout(function () {
     box.style.setProperty("transition", "initial");
     box.style.transform = "rotate(90deg)";
